64c8544065f13f761b3af914d25b1115bc60f3d0
LJason <ljason@ljason.cn>
2021-10-07 20:39:15 +0800

    修复 clippy 报错
    Signed-off-by: LJason <ljason@ljason.cn>

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use rocket::{catchers, Build, Rocket};
 pub mod catchers;
 pub mod models;
 
+#[must_use]
 pub fn rocket() -> Rocket<Build> {
     rocket::build().register("/", catchers![catchers::not_found])
 }
